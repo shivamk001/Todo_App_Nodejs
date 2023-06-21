8d7a6cab90ae8c94858a3eeff3b7e1f548640ae7
Shivam Kesarwani <shivamkesarwani001@gmail.com>
2023-06-21 17:22:02 +0530

    Added assets folder for static files, homeController.js, home.ejs in views

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app=express()
 const port=8000
 const routes=require('./routes')
 
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('assets'))
+
 app.use('/', routes)
 
 app.listen(port, function(err){
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express=require('express')
 const router=express.Router()
 
-router.get('/', function(req, res){
-    return res.end('<h1>Testing</h1>')
-})
+const homeController=require('../controllers/homeController')
+
+router.get('/', homeController.home)
 
 module.exports=router
